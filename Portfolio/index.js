20578ee813babcba2d4440e7c40eee906590b587
03-Omkar <omkarbagad03@gmail.com>
2023-09-29 17:32:01 +0530

    Initial commit

---
+++ Portfolio/index.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".role",{
+    strings: [
+        "Frontend Developer",
+        "Web Developer",
+        "Coder",
+        "Tech Enthusiast"
+    ],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:80,
+    backDelay: 1000,
+});
